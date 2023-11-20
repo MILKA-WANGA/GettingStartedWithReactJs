cfb784e83dff3536d67f192cd8a15d83f1631d20
Milka Wanga <bakhuyamilka@gmail.com>
2023-11-20 07:57:27 +0300

    Used a different name for imported component in default export

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,12 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
 
-import Greets from './component/Greets';
+import Hello from './component/Greets';
 
 function App() {
   return (
     <div className="App">
-      <Greets/>
+      <Hello/>
     </div>
   );
 }
